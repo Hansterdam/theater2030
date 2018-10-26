910d4c5fd8d857d74f6971192c3e41d72158058b
Han <han@dedact.nl>
2018-10-26 22:44:59 +0200

    Add persistence

---
+++ serviceapi/persistence.py
@@ -0,0 +1,42 @@
+halls = [
+    {'id': 1, 'name': 'Grote zaal'},
+    {'id': 2, 'name': 'Kleine zaal'},
+]
+
+seats = [
+    {'id': 1, 'hall_id': 1, 'row': 'A', 'number': 1},
+    {'id': 2, 'hall_id': 1, 'row': 'A', 'number': 2},
+    {'id': 3, 'hall_id': 1, 'row': 'A', 'number': 3},
+    {'id': 4, 'hall_id': 1, 'row': 'A', 'number': 4},
+    {'id': 5, 'hall_id': 1, 'row': 'B', 'number': 1},
+    {'id': 6, 'hall_id': 1, 'row': 'B', 'number': 2},
+    {'id': 7, 'hall_id': 1, 'row': 'B', 'number': 3},
+    {'id': 8, 'hall_id': 1, 'row': 'B', 'number': 4},
+    {'id': 9, 'hall_id': 1, 'row': 'C', 'number': 1},
+    {'id': 10, 'hall_id': 1, 'row': 'C', 'number': 2},
+    {'id': 11, 'hall_id': 1, 'row': 'C', 'number': 3},
+    {'id': 12, 'hall_id': 1, 'row': 'C', 'number': 4},
+    {'id': 13, 'hall_id': 2, 'row': 'A', 'number': 1},
+    {'id': 14, 'hall_id': 2, 'row': 'A', 'number': 2},
+    {'id': 15, 'hall_id': 2, 'row': 'A', 'number': 3},
+    {'id': 16, 'hall_id': 2, 'row': 'B', 'number': 1},
+    {'id': 17, 'hall_id': 2, 'row': 'B', 'number': 2},
+    {'id': 18, 'hall_id': 2, 'row': 'B', 'number': 3},
+]
+
+films = [
+    {'id': 1, 'title': 'Her', 'length': 126},
+    {'id': 2, 'title': 'Leaning Into the Wind', 'length': 97},
+]
+
+shows = [
+    {'id': 1, 'film_id': 1, 'hall_id': 1, 'time': '14:00'},
+    {'id': 2, 'film_id': 2, 'hall_id': 2, 'time': '14:30'},
+    {'id': 3, 'film_id': 2, 'hall_id': 1, 'time': '16:30'},
+    {'id': 4, 'film_id': 1, 'hall_id': 2, 'time': '16:45'},
+    {'id': 5, 'film_id': 1, 'hall_id': 1, 'time': '18:30'},
+    {'id': 6, 'film_id': 2, 'hall_id': 2, 'time': '19:15'},
+    {'id': 7, 'film_id': 2, 'hall_id': 1, 'time': '21:00'},
+    {'id': 8, 'film_id': 1, 'hall_id': 2, 'time': '21:15'},
+]
+
